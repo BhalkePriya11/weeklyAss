4b0f3b4bc080c5a4a91ae0671b77906890647f37
Bhalke@priya <97160531+BhalkePriya11@users.noreply.github.com>
2022-03-04 23:30:23 +0530

    Add files via upload

---
+++ feb28-mar4/feb28/fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzbuzz(n) {
+    let fizzBuzz = prompt("Enter Your Limit");
+    (n != NaN)
+    document.getElementById("AA").innerHTML = fizzBuzz + " is not integer....please refresh and enter an integer value";
+    for (i = 1; i <= fizzBuzz; i++) {
+        if (i % 3 === 0 && i % 5 === 0)
+            document.write("fizzbuzz" + " ")
+        else if (i % 3 === 0)
+            document.write("fizz" + " ");
+        else if (i % 5 === 0)
+            document.write("buzz" + " ");
+        else
+            document.write(i + " ");
+    }
+    document.write("<hr />" + "You can play again")
+}
